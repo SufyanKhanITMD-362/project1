2f3cdd3cd608fdac207d18765e5131e2e8c158c9
skhan125 <112320594+skhan125@users.noreply.github.com>
2023-03-29 06:11:44 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,13 @@
-// jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-	$('html').removeClass('nojs');
-	$('html').addClass('hasjs');
-});
+<button id="submit-btn">Create Account</button>
+
+<script>
+  document.getElementById("submit-btn").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevents the default form submission behavior
+
+    // TODO: Add validation logic here
+
+    // If the form is valid, submit it
+    document.getElementById("signup-form").submit();
+  });
+</script>
+
